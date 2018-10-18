914d767775224ddba66e4e71ff58f1c75850a82c
Thomas G <sio.galibert@gmail.com>
2018-10-18 17:56:08 +0200

    Mise en place du routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { PanelAjoutVideoComponent } from './panel-ajout-video/panel-ajout-video.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'recettes', component: PanelAjoutVideoComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/panel-ajout-video/panel-ajout-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-ajout-video',
+  templateUrl: './panel-ajout-video.component.html',
+  styleUrls: ['./panel-ajout-video.component.css']
+})
+export class PanelAjoutVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
