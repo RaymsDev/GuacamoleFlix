7437e04984e22c38e8750679629f0e7a5c654544
Yseult Pissama <yseult.pissama.external@sigfox.com>
2018-10-25 14:45:24 +0200

    register component

---
+++ client/src/app/ressources/register/register.component.ts
@@ -0,0 +1,32 @@
+import {FormControl, Validators} from '@angular/forms';
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+
+  ngOnInit() {
+  }
+  loginFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  register(email, password){
+    console.log('register ts', email, password);
+
+    return this.authService.register(email, password);
+  }
+  loginGoogle(){
+    console.log('login google');
+
+    return this.authService.loginGoogle();
+  }
+
+}
