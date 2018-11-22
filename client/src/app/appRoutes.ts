105b1e542164b54987c8b9865d1a29001217fd8a
Yseult Pissama <yseult.pissama.external@sigfox.com>
2018-11-22 17:20:23 +0100

    style

--- client/src/app/appRoutes.ts
+++ client/src/app/appRoutes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './ressources/login/login.component';
 import { RegisterComponent } from './ressources/register/register.component';
 import { AppComponent } from './app.component';
-import { AjoutVideoAdminComponent } from './ressources/ajout-video-admin/ajout-video-admin.component'
-import { GestionUsersComponent } from './ressources/gestion-users/gestion-users.component'
-import { GestionVideosComponent } from './ressources/gestion-videos/gestion-videos.component'
+import { AjoutVideoAdminComponent } from './ressources/ajout-video-admin/ajout-video-admin.component';
+import { GestionUsersComponent } from './ressources/gestion-users/gestion-users.component';
+import { GestionVideosComponent } from './ressources/gestion-videos/gestion-videos.component';
 import { HomePageComponent } from './ressources/home-page/home-page.component';
 import { UserComponent } from './ressources/user/user.component';
 
