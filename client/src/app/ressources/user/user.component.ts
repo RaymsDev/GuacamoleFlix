c62db7d6f582b2a1ff379bcf7d3392cba434202c
Yseult Pissama <yseult.pissama.external@sigfox.com>
2018-12-06 17:00:22 +0100

    add user profile

--- client/src/app/ressources/user/user.component.ts
+++ client/src/app/ressources/user/user.component.ts
@@ -10,23 +10,67 @@ import { Observable } from 'rxjs';
 })
 export class UserComponent implements OnInit {
 
-  userObservable: any;
+  userObservable: Observable<any>;
   constructor(public authService: AuthService, public userService: UserService) { }
   users;
 
   ngOnInit() {
-    this.userObservable = this.authService.getUser().subscribe(e => {
-      return e;
+    this.userObservable = this.authService.getUser();
+    this.authService.getUser().subscribe(data => {
+      this.getCurrentUser(data.uid);
+
     });
-    console.log(this.userObservable);
-    this.getUsers(this.userObservable.uid);
   }
   getUserAuth() {
     return this.authService.getUser();
   }
-  getUsers(firebaseId) {
-  this.userService.getCurrentUser(firebaseId).subscribe((data) => {
+  getUsers() {
+  this.userService.getUsers().subscribe((data) => {
       this.users = data; } );
+    console.log('this.users');
+    console.log(this.users);
+
+  }
+  getCurrentUser(firebaseId) {
+  this.userService.getCurrentUser(firebaseId).subscribe((data) => {
+      this.users = data.data; } );
+
   }
 
 }
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { AuthService } from '../../services/auth.service';
+// import { UserService } from '../../services/user.service';
+// import { Observable } from 'rxjs';
+
+// @Component({
+//   selector: 'app-user',
+//   templateUrl: './user.component.html',
+//   styleUrls: ['./user.component.scss']
+// })
+// export class UserComponent implements OnInit {
+
+//   userObservable: any;
+//   constructor(public authService: AuthService, public userService: UserService) { }
+//   users;
+
+//   ngOnInit() {
+//     this.userObservable = this.authService.getUser().subscribe(e => {
+//       return e;
+//     });
+//     console.log(this.userObservable);
+//     this.getUsers(this.userObservable.uid);
+//   }
+//   getUserAuth() {
+//     return this.authService.getUser();
+//   }
+//   getUsers(firebaseId) {
+//   this.userService.getCurrentUser(firebaseId).subscribe((data) => {
+//       this.users = data; } );
+//   }
+
+// }
